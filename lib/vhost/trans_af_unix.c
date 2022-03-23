ndragazis/dpdk-next-virtio

vhost: remove main fd parameter from msg handlers
After refactoring the socket I/O and the vhost-user map/unmap operations
in previous patches, there is no need for the connection fds in the core
vhost-user code. This patch removes the connection fds from the core
vhost-user code.

Connection fds are used for socket I/O between master and slave.
However, this mechanism is transport-specific. Other transports may use
other mechanisms for the master/slave communication. Therefore, the
connection fds are moved into the AF_UNIX transport code.

Signed-off-by: Nikos Dragazis <ndragazis@arrikto.com>

lib/librte_vhost/trans_af_unix.c
@@ -374,7 +374,7 @@ vhost_user_read_cb(int connfd, void *dat, int *remove)
 		goto err;
 	}
 
-	ret = vhost_user_msg_handler(conn->device.vid, connfd, &msg);
+	ret = vhost_user_msg_handler(conn->device.vid, &msg);
 	if (ret < 0) {
 err:
 		close(connfd);

lib/librte_vhost/vhost_user.h
@@ -146,7 +146,7 @@ typedef struct VhostUserMsg {
 
 
 /* vhost_user.c */
-int vhost_user_msg_handler(int vid, int fd, const struct VhostUserMsg *msg);
+int vhost_user_msg_handler(int vid, const struct VhostUserMsg *msg);
 int add_guest_pages(struct virtio_net *dev,
 		   struct rte_vhost_mem_region *reg,
 		   uint64_t page_size);

uarif1/dpdk

lib/vhost/trans_af_unix.c
@@ -403,7 +403,7 @@ vhost_user_read_cb(int connfd, void *dat, int *remove)
 		goto err;
 	}
 
-	ret = vhost_user_msg_handler(conn->device.vid, connfd, &ctx);
+	ret = vhost_user_msg_handler(conn->device.vid, &ctx);
 	if (ret < 0) {
 err:
 		close(connfd);

lib/vhost/vhost_user.h
@@ -168,7 +168,7 @@ struct __rte_packed vhu_msg_context {
 
 
 /* vhost_user.c */
-int vhost_user_msg_handler(int vid, int fd, const struct vhu_msg_context *ctx_);
+int vhost_user_msg_handler(int vid, const struct vhu_msg_context *ctx_);
 int add_guest_pages(struct virtio_net *dev,
 		   struct rte_vhost_mem_region *reg,
 		   uint64_t page_size);